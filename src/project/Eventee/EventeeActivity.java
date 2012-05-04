529dcf80f0a41c9f63697680e03fbd8191c76792
Greg Dubelko <greg.dubelko@gmail.com>
2012-05-04 15:17:37 -0400

    UI commit2

--- src/project/Eventee/EventeeActivity.java
+++ src/project/Eventee/EventeeActivity.java
@@ -2,6 +2,8 @@ package project.Eventee;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EventeeActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,6 +11,14 @@ public class EventeeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
-    
-}
+        
+        final Button button = (Button) findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() 
+        {
+            public void onClick(View v)
+            {
+            	setContentView(R.layout.seek);
+            }
+          });
+      }
+  }
