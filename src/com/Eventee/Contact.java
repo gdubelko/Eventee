7afce9022c3273b29c0880086fd09a7afb236981
Greg Dubelko <greg.dubelko@gmail.com>
2012-05-21 23:01:55 -0400

    Ready for test day

---
+++ src/com/Eventee/Contact.java
@@ -0,0 +1,27 @@
+package com.Eventee;
+
+public class Contact 
+{
+	private String name;
+	private String details;
+	
+	public String getContactName()
+	{
+		return this.name;
+	}
+
+	public String getContactDetails() 
+	{
+		return this.details;
+	}
+
+	public void setContactName(String name) 
+	{
+		this.name = name;
+	}
+
+	public void setContactDetails(String details) 
+	{
+		this.details = details;
+	}
+}
